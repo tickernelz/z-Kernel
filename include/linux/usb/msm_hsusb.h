a1b66760d08e9a3f00f8ca8b702934e36b3cdca3
Haruka <ichihara.rifa@gmail.com>
2016-03-30 09:24:13 +0700

    "Add USB Fast Charging (and MAX mA set to 1500 if activated)"

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -105,19 +105,26 @@ enum msm_usb_phy_type {
 	SNPS_FEMTO_PHY,
 };
 #if defined(WT_USE_86519_CHARGE_1200)
-#define IDEV_CHG_MAX	1200
+#define IDEV_CHG_MAX	2000
 #elif defined(WT_USE_S86001_CHARGE_1000)
-#define IDEV_CHG_MAX	1000
+#define IDEV_CHG_MAX	2000
 #elif defined(CONFIG_WT_USE_XIAOMI_CHARGE_1050)
-#define IDEV_CHG_MAX	1050
+#define IDEV_CHG_MAX	2000
 #else
 #define IDEV_CHG_MAX	2000
+
+#ifdef CONFIG_MACH_WT88047
+#define IDEV_CHG_MAX		1050
+#define IDEV_CHG_MAX_FAST	2000
+#else
+#define IDEV_CHG_MAX		2000
+#define IDEV_CHG_MAX_FAST	2000
 #endif
 
 #define IDEV_CHG_MIN	500
 #define IUNIT		100
 
-#define IDEV_ACA_CHG_MAX	1500
+#define IDEV_ACA_CHG_MAX	2000
 #define IDEV_ACA_CHG_LIMIT	500
 
 /**
